051f8c8f8ad81c971a138167ad348d7ddaf1dd9d
carraes <carraeshb@gmail.com>
2023-01-19 19:31:49 -0300

    feat: add l. and ld

--- fish/.config/fish/conf.d/myfish.fish
+++ fish/.config/fish/conf.d/myfish.fish
@@ -9,12 +9,15 @@ alias lg=lazygit
 # NeoVim
 alias vi=nvim
 alias vim=nvim
+alias setlint='nvim ~/.config/nvim/lua/plugins/lsp/null-ls.lua'
 
 # ls
 alias ls='exa -al --color=always --group-directories-first'
 alias la='exa -a --color=always --group-directories-first'
 alias ll='exa -l --color=always --group-directories-first'
 alias lt='exa -aT --color=always --group-directories-first'
-   
+alias l.='exa -als mod --color=always --group-directories-first -I .git'
+alias ld='exa -als mod --group-directories-first --color=always'
+
 # Go
 alias ghtml='go test -coverprofile=coverage.out && go tool cover -html=coverage.out'
