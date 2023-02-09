60b7e6ade5b8c852d740de9b287424c39b53ad65
carraes <carraeshb@gmail.com>
2023-02-09 00:16:36 -0300

    feat: copilot.lua, remove copilot.vim

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,48 @@
+local copilot_setup, copilot = pcall(require, "copilot")
+if not copilot_setup then
+	return
+end
+
+copilot.setup({
+	panel = {
+		enabled = true,
+		auto_refresh = false,
+		keymap = {
+			jump_prev = "[[",
+			jump_next = "]]",
+			accept = "<CR>",
+			refresh = "gr",
+			open = "<M-CR>",
+		},
+		layout = {
+			position = "bottom", -- | top | left | right
+			ratio = 0.4,
+		},
+	},
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
+		debounce = 75,
+		keymap = {
+			accept = "<M-1>",
+			accept_word = false,
+			accept_line = false,
+			next = "<M-2>",
+			prev = "<M-3>",
+			dismiss = "<C-]>",
+		},
+	},
+	filetypes = {
+		yaml = false,
+		markdown = false,
+		help = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+	},
+	copilot_node_command = "node", -- Node.js version must be > 16.x
+	server_opts_overrides = {},
+})
