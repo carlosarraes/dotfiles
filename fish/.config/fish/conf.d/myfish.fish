461a9e51cdb01e327bf84061282eaf32972d896d
carraes <carraeshb@gmail.com>
2023-03-03 01:07:17 -0300

    feat: noto fonts alias, lda command, cleanup docker alias

--- fish/.config/fish/conf.d/myfish.fish
+++ fish/.config/fish/conf.d/myfish.fish
@@ -8,18 +8,22 @@ alias lzg=lazygit
 alias gui=gitui
 alias lzd='sudo lazydocker'
 alias conky='conky -c ~/.config/conky/mocha.conf'
+
+# Fixes and Installers
 alias fixkeyboard='setxkbmap -rules evdev -model pc105+inet -layout us -variant intl'
+alias setupnoto='sudo pacman -S $(pacman -Ssq "noto-fonts-*")'
 
 # Docker
-alias cleardocker='docker system prune -af'
-alias dockerls='docker container ls -a'
-alias dockerils='docker image ls -a'
-alias cdockerup='docker-compose up -d'
-alias cdockerdown='docker-compose down'
-alias cdockerlogs='docker-compose logs -f'
+alias dclear='docker system prune -af'
+alias dls='docker container ls -a'
+alias dils='docker image ls -a'
+alias dcup='docker-compose up -d'
+alias dcdown='docker-compose down'
+alias dclogs='docker-compose logs -f'
 
 # OpenSSH
 alias connectplex='ssh plex@192.168.15.45'
+alias mapnetwork='sudo nmap -sP 192.168.15.0/24'
 
 # Note
 alias screenoff='xrandr --output eDP --off'
@@ -37,7 +41,8 @@ alias la='exa -a --color=always --group-directories-first'
 alias ll='exa -l --color=always --group-directories-first'
 alias lt='exa -aT --color=always --group-directories-first'
 alias l.='exa -als mod --color=always --group-directories-first -I .git'
-alias ld='exa -als mod --group-directories-first --color=always'
+alias ld='exa -ls mod --group-directories-first --color=always'
+alias lda='exa -als mod --group-directories-first --color=always'
 
 # Go
 alias ghtml='go test -coverprofile=coverage.out && go tool cover -html=coverage.out'
