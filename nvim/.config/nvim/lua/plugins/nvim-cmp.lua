7c5e295e19a878fa4e8de437b0f73ec2adfd6e09
carraes <carraeshb@gmail.com>
2023-07-02 11:28:53 -0300

    feat(nvim): cmp configs

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -50,6 +50,18 @@ cmp.setup({
 			ellipsis_char = "...",
 		}),
 	},
+	sorting = {
+		comparators = {
+			cmp.config.compare.offset,
+			cmp.config.compare.exact,
+			cmp.config.compare.score,
+			cmp.config.compare.recently_used,
+			cmp.config.compare.kind,
+			cmp.config.compare.sort_text,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	},
 })
 
 vim.keymap.set({ "i", "s" }, "<C-k>", function()
