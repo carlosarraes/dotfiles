a6a1419162b4edf6c34c99e8c583b7b11edb290b
carraes <carraeshb@gmail.com>
2024-09-29 18:24:53 -0300

    feat(nvim): adjust flash, lualine and add multicursor plugin

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -15,6 +15,13 @@ return {
 				section_separators = "",
 			},
 			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
 				lualine_c = {
 					{
 						function()
