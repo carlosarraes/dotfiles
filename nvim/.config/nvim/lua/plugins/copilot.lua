42dd751a67755868b8fd6d22bea912652e058f7f
carraes <carraeshb@gmail.com>
2025-01-02 10:28:22 -0300

    feat(nvim): change configs and format of copilot/blink

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,54 +1,54 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    if vim.g.vscode then
-      return
-    end
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		if vim.g.vscode then
+			return
+		end
 
-    local copilot = require("copilot")
+		local copilot = require("copilot")
 
-    copilot.setup({
-      panel = {
-        enabled = true,
-        auto_refresh = false,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept = "<CR>",
-          refresh = "gr",
-          open = "<M-4>",
-        },
-        layout = {
-          position = "bottom",
-          ratio = 0.4,
-        },
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = false,
-        debounce = 75,
-        keymap = {
-          accept = "<Tab>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-s>",
-          prev = "<C-e>",
-          dismiss = "<C-]>",
-        },
-      },
-      filetypes = {
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
-      },
-      copilot_node_command = "node",
-      server_opts_overrides = {},
-    })
-  end,
+		copilot.setup({
+			panel = {
+				enabled = true,
+				auto_refresh = false,
+				keymap = {
+					jump_prev = "[[",
+					jump_next = "]]",
+					accept = "<CR>",
+					refresh = "gr",
+					open = "<M-4>",
+				},
+				layout = {
+					position = "bottom",
+					ratio = 0.4,
+				},
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = false,
+				debounce = 75,
+				keymap = {
+					accept = "<C-y>",
+					accept_word = false,
+					accept_line = false,
+					next = "<C-s>",
+					prev = "<C-e>",
+					dismiss = "<C-]>",
+				},
+			},
+			filetypes = {
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+				["."] = false,
+			},
+			copilot_node_command = "node",
+			server_opts_overrides = {},
+		})
+	end,
 }
