5e62ad1346ba8b083b9d144e22e09ca5096db2ef
carraes <carraeshb@gmail.com>
2025-02-04 09:48:41 -0300

    feat(nvim): fully migrate to snacks picker

--- nvim/.config/nvim/lua/plugins/lsp/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp.lua
@@ -26,9 +26,10 @@ return {
 			bind("n", "gd", function()
 				vim.lsp.buf.definition({ on_list = on_list })
 			end, opts) -- go to definition
-			bind("n", "gr", "<cmd>lua require('fzf-lua').lsp_references()<cr>", opts) -- show references
-			bind("n", "gi", "<cmd>lua require('fzf-lua').lsp_implementations()<cr>", opts) -- show references
-			bind("n", "gt", "<cmd>lua require('fzf-lua').lsp_typedefs()<cr>", opts) -- show references
+			bind("n", "gD", ":lua Snacks.picker.lsp_declarations()<cr>", opts) -- go to declaration
+			bind("n", "gr", ":lua Snacks.picker.lsp_references()<cr>", opts) -- show references
+			bind("n", "gi", ":lua Snacks.picker.lsp_implementations()()<cr>", opts) -- show references
+			bind("n", "gt", ":lua Snacks.picker.lsp_type_definitions()<cr>", opts) -- show references
 			bind("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 			bind({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
 			bind("n", "<space>rn", vim.lsp.buf.rename, opts)
