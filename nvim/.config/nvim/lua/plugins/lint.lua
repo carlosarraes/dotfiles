10058b8d60e7f958084fb1b35613fab7bd1ccadf
carraes <carraeshb@gmail.com>
2025-03-02 17:13:54 -0300

    feat(nvim): update lint to use biome instead of eslint

--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -1,49 +1,49 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = {
-    "BufReadPre",
-    "BufNewFile",
-  },
-  config = function()
-    if vim.g.vscode then
-      return
-    end
+	"mfussenegger/nvim-lint",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+	config = function()
+		if vim.g.vscode then
+			return
+		end
 
-    local lint = require("lint")
-    local eslint = lint.linters.eslint_d
+		local lint = require("lint")
+		local eslint = lint.linters.eslint_d
 
-    lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      python = { "mypy" },
-    }
-    eslint.args = {
-      "--no-warn-ignored",
-      "--format",
-      "json",
-      "--stdin",
-      "--stdin-filename",
-      function()
-        return vim.api.nvim_buf_get_name(0)
-      end,
-    }
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		lint.linters_by_ft = {
+			javascript = { "biomejs" },
+			typescript = { "biomejs" },
+			javascriptreact = { "biomejs" },
+			typescriptreact = { "biomejs" },
+			python = { "mypy" },
+		}
+		eslint.args = {
+			"--no-warn-ignored",
+			"--format",
+			"json",
+			"--stdin",
+			"--stdin-filename",
+			function()
+				return vim.api.nvim_buf_get_name(0)
+			end,
+		}
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-    vim.api.nvim_create_autocmd({
-      "BufEnter",
-      "BufWritePost",
-      "InsertLeave",
-    }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
+		vim.api.nvim_create_autocmd({
+			"BufEnter",
+			"BufWritePost",
+			"InsertLeave",
+		}, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
 
-    vim.keymap.set("n", "<leader>l", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting" })
-  end,
+		vim.keymap.set("n", "<leader>l", function()
+			lint.try_lint()
+		end, { desc = "Trigger linting" })
+	end,
 }
