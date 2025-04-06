76439abc4ac8d811f314793ade4e47a011535b15
carraes <carraeshb@gmail.com>
2025-04-06 20:43:41 -0300

    feat(nvim): adjust typescript lsp

--- nvim/.config/nvim/lua/plugins/lsp/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp.lua
@@ -35,20 +35,24 @@ return {
 			bind("n", "<space>rn", vim.lsp.buf.rename, opts)
 			bind("n", "<C-p>", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
 			bind("n", "<C-n>", "<cmd>lua vim.diagnostic.goto_next()<CR>")
-
-			bind("n", "<leader>rf", ":VtsExec rename_file<CR>") -- rename file and update imports
-			bind("n", "<leader>ri", ":VtsExec remove_unused_imports<CR>") -- remove unused variables
-			bind("n", "<leader>ru", ":VtsExec organize_imports<CR>") -- organize imports
 			if client.server_capabilities.documentSymbolProvider then
 				navic.attach(client, bufnr)
 			end
 		end
 
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		vim.diagnostic.config({
+			signs = {
+				active = {
+					{ name = "DiagnosticSignError", text = " " },
+					{ name = "DiagnosticSignWarn", text = " " },
+					{ name = "DiagnosticSignHint", text = "󰠠 " },
+					{ name = "DiagnosticSignInfo", text = " " },
+				},
+			},
+			-- other options you might already be using, e.g.:
+			virtual_text = false,
+			severity_sort = true,
+		})
 
 		lspconfig["html"].setup({
 			capabilities = capabilities,
@@ -58,40 +62,6 @@ return {
 		lspconfig["marksman"].setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
-		})
-
-		lspconfig["vtsls"].setup({
-			server = {
-				capabilities = capabilities,
-				on_attach = on_attach,
-				settings = {
-					complete_function_calls = true,
-					vtsls = {
-						enableMoveToFileCodeAction = true,
-						autoUseWorkspaceTsdk = true,
-						experimental = {
-							maxInlayHintLength = 30,
-							completion = {
-								enableServerSideFuzzyMatch = true,
-							},
-						},
-					},
-					typescript = {
-						updateImportsOnFileMove = { enabled = "always" },
-						suggest = {
-							completeFunctionCalls = true,
-						},
-						inlayHints = {
-							enumMemberValues = { enabled = true },
-							functionLikeReturnTypes = { enabled = true },
-							parameterNames = { enabled = "literals" },
-							parameterTypes = { enabled = true },
-							propertyDeclarationTypes = { enabled = true },
-							variableTypes = { enabled = false },
-						},
-					},
-				},
-			},
 		})
 
 		lspconfig["cssls"].setup({
