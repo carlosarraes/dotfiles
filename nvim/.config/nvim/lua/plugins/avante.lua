54cdc42f0f66ce89fd202de806adc83623c5213d
carraes <carraeshb@gmail.com>
2025-04-28 16:50:58 -0300

    feat(nvim): back to avante

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,15 @@
+return {
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	version = false,
+	opts = {
+		provider = "claude",
+	},
+	build = "make",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+	},
+}
