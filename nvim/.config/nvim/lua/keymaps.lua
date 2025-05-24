6a1b535b221b4969bbf21d50dd8353acf99e41e5
carraes <carraeshb@gmail.com>
2025-05-23 22:07:05 -0300

    feat(nvim): remove local term

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -102,6 +102,3 @@ bind("n", "<leader>e", ":lua MiniFiles.open()<cr>", opts)
 -- MCPHub
 bind("n", ";h", ":MCPHub<cr>", opts)
 
-local termtoggle = require("local.term")
-bind("n", "<C-t>", termtoggle.toggleterm, { desc = "toggle terminal" })
-bind("t", "<C-t>", termtoggle.toggleterm, { buffer = termtoggle.buf, desc = "toggle terminal" })
